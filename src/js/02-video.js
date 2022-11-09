0f8b044e2d25811bc6a84b0dd594b51d5e2eeb1a
Anastasiia Sukhorukova <hrlnqnn@gmail.com>
2022-11-09 18:40:46 +0200

    added Vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const trottle = require('lodash.throttle');
+
+function getVideoTime(e) {
+    localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
+
+player.on('timeupdate', trottle(getVideoTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
