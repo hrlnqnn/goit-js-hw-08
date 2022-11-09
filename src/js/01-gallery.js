f33417b9611aae4c76b9149e724be4cb48d9c48c
Anastasiia Sukhorukova <hrlnqnn@gmail.com>
2022-11-09 18:10:45 +0200

    added SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('div.gallery');
+
+function createGallery(images) {
+    return images
+        .map(({ original, preview, description }) => {
+            return `
+      <div class="gallery">
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </div>`;
+        })
+        .join('');
+}
+
+const imageMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
+
+const lightbox = new SimpleLightbox('div.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
